109f377c8b18d3fe7b8023d85525a0fe2445ef28
jcdeguzman <jeremiah.dg17@gmail.com>
2023-05-11 10:36:12 +0800

    edited map

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ import TimelinePage from "./TimelinePage";
 
 const Main = () => {
 
-    const [zoomedLocation, setZoomedLocation] = useState({lat: 11.15405761270903, lng: 122.48382568359376})
+    const [zoomedLocation, setZoomedLocation] = useState({lat: 11.15405761270904, lng: 122.48382568359376})
     const [zoom, setZoom] = useState(9)
     const [mapType, setMapType] = useState("street");
     const [sites, setSites] = useState({
